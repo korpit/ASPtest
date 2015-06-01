88ebf8fc0f81b6f90301461f8d4d41c490a422d2
Timo Korpinen <timo.korpinen@inpulse.fi>
2015-06-01 17:06:20 +0300

    GH_testausta
    GitHub testausta

---
+++ HarjoitusTK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HarjoitusTK.Startup))]
+namespace HarjoitusTK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
