ee1ea041eb4939593a1abe4cefadfaaf93a93871
Timo Korpinen <timo.korpinen@inpulse.fi>
2015-06-02 15:47:22 +0300

    migration
    added AddBlogUrl -class
    ...

---
+++ EntityMVC/EntityMVC/Migration/AddBlogUrl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity.Migrations
+
+namespace EntityMVC.Migration
+{
+    public partial class AddBlogUrl : DbMigration
+    {
+        public override void Up()
+        {
+ 	        AddColumn("dbo.Blogs","Url",c=> c.String());
+        }
+        public override void Down()
+        {
+ 	        DropColumn("dbo.Blogs","Url");
+        }
+    }
+
+
+
+}
+
